f6d5bb17251c297b3e4734604b832625955b96b6
Aman <88755081+amanTHEBreaker@users.noreply.github.com>
2021-08-25 14:26:12 -0700

    description added

--- src/main/kotlin/Program1.kt
+++ src/main/kotlin/Program1.kt
@@ -1,3 +1,8 @@
+// main is the entry point of program
+// parameters in main are the arguments which we can supply
+// through command line
+
+
 fun main(args : Array<String>) {
     println("Name : Aman");
     print("Age : 21")
