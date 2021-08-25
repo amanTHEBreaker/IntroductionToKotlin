72eb636aa9ede4356154651ccfa7030f70fb18a3
Aman <88755081+amanTHEBreaker@users.noreply.github.com>
2021-08-25 14:24:50 -0700

    11th ans added

---
+++ src/main/kotlin/Program11.kt
@@ -0,0 +1,18 @@
+import java.util.*;
+
+fun main(args: Array<String>) {
+
+}
+
+
+/*
+* Basic syntax of class
+*/
+
+// Class declaration
+class Developer {
+
+    lateinit var name: String
+    lateinit var lang: String
+
+}
